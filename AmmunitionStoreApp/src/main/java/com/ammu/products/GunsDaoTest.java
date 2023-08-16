889eb16faf31d82040b5c054518bba911ac78ece
Rakshitha <103557129+rakshithar23@users.noreply.github.com>
2023-08-16 11:08:37 +0530

    Update GunsDaoTest.java

--- AmmunitionStoreApp/src/main/java/com/ammu/products/GunsDaoTest.java
+++ AmmunitionStoreApp/src/main/java/com/ammu/products/GunsDaoTest.java
@@ -5,7 +5,7 @@ public class GunsDaoTest {
     public void testSelect() {
     	GunsDao guns = GunsDao.getInstance();
         guns.select();
-        // add assert statements to verify the output of the select method
+       
     }
 }
 
